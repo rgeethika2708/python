2071ce4ecdb9982127816bb69e586d6255f524c8
rgeethika2708 <rgeethika2708@gmail.com>
2026-08-03 22:33:20 +0530

    Create Complex numbers.py

---
+++ chapter1/Complex numbers.py
@@ -0,0 +1,8 @@
+a=input("enter real part of first complex number:")
+b=input("enter imaginary part of first complex number:")
+c=input("enter real part of second complex number:")
+d=input("enter imaginary part of second complex number:")
+c1=complex(a,b)
+c2=complex(c,d)
+sum=c1+c2
+print("sum=",sum)
