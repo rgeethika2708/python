4f352f6118ea368b87798ea642a7021d7c6f09b5
rgeethika2708 <rgeethika2708@gmail.com>
2026-08-11 17:30:30 +0530

    Add script to calculate product of complex numbers

---
+++ chapter1/product of complex numbers.py
@@ -0,0 +1,8 @@
+a=int(input("enter real part of first complex number:"))
+b=int(input("enter imaginary part of first complex number;"))
+c=int(input("enter real part of second complex number:"))
+d=int(input("enter imaginary part of second complex number:"))
+c1=(a,b)
+c2=(c,d)
+product=c1*c2
+print("product=",product)
