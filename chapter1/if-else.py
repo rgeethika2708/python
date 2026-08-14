7dc03ded4118b9b4c0dc8ddbcf211ed8f65377cf
rgeethika2708 <rgeethika2708@gmail.com>
2026-08-14 19:18:09 +0530

    Add if-else calculator in if-else.py

---
+++ chapter1/if-else.py
@@ -0,0 +1,11 @@
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+n=int(input("enter number"))
+if n==1:
+    print(a+b)
+elif n==2:
+    print(a-b)
+elif n==3:
+    print(a*b)
+else:
+    print("invalid number")            
