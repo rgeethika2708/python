01573bfbdfb2c43196c60a44850bda8f8cd0f63f
rgeethika2708 <rgeethika2708@gmail.com>
2026-09-10 11:05:15 +0530

    Add integer to Roman numeral conversion script

---
+++ chapter1/integer to roman.py
@@ -0,0 +1,10 @@
+num=int(input("enter an integer:"))
+values=[1000,900,500,400,100,90,50,40,10,9,5,4,1]
+symbols=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+roman=" "
+for i in range(len(values)):
+  while num>=values[i]:
+    roman+=symbols[i]
+    num-=values[i]
+print("roman numeral:",roman)
+     
