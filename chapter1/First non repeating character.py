64aa8503b0c7dc561a675ab797372cd375062725
rgeethika2708 <rgeethika2708@gmail.com>
2026-09-17 21:28:40 +0530

    Add script to find first non-repeating character

---
+++ chapter1/First non repeating character.py
@@ -0,0 +1,8 @@
+s = input("Enter a string: ")
+
+for ch in s:
+    if s.count(ch) == 1:
+        print("First non-repeating character:", ch)
+        break
+else:
+    print("No non-repeating character")
