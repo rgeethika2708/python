e87bf7f73b4c4cad4ccebadd2e91c13e1eb63c58
rgeethika2708 <rgeethika2708@gmail.com>
2026-09-18 20:57:47 +0530

    Implement basic stack operations in stack.py

---
+++ chapter1/stack.py
@@ -0,0 +1,8 @@
+stack=[]
+stack.append(10)
+stack.append(20)
+stack.append(30)
+print("stack",stack)
+print("popped element=",stack.pop())
+print("stack after pop",stack)
+print("top element=",stack[-1])
