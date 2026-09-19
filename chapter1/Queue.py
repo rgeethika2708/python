36836c413d82d91d026371f7d2ddd82dbeacbcd7
rgeethika2708 <rgeethika2708@gmail.com>
2026-09-19 23:33:44 +0530

    Add Queue implementation in Queue.py

---
+++ chapter1/Queue.py
@@ -0,0 +1,8 @@
+queue = []
+queue.append(10)
+queue.append(20)
+queue.append(30)
+print("Queue:", queue)
+print("Deleted element:", queue.pop(0))
+print("Queue after deletion:", queue)
+print("Front element:", queue[0])
